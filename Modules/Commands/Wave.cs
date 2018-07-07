6c133a75253ddc0948d2249e2bae029c8575d7b2
thesackofeyes <iamthompson96@gmail.com>
2018-07-07 17:07:29 +0100

    Updating .NET Framework

---
+++ Modules/Commands/Wave.cs
@@ -0,0 +1,26 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class wave : ModuleBase<SocketCommandContext>
+    {
+        [Command("o/")]
+        public async Task PingAsync()
+        {
+            String[] waves = ["\o","o7","\o/"];
+            ArrayList<String> waves = new ArrayList<String>();
+            waves.add("o/");
+            waves.add("\o");
+            waves.add("o7");
+            waves.add("\o/");
+            Collections.shuffle(waves);
+            String reply = waves.get(0);
+            await ReplyAsync(reply);
+        }
+    }
+}
