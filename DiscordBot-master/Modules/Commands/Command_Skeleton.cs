5972bce49f7a691c3bff8ba4a700a31c818be631
andrew <betto2009@live.co.uk>
2018-07-07 20:58:34 +0100

    Andy test commit

---
+++ DiscordBot-master/Modules/Commands/Command_Skeleton.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class COMMAND_NAME : ModuleBase<SocketCommandContext>
+    {
+        [Command("COMMAND_NAME")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("COMMAND_DOES");
+        }
+    }
+}
