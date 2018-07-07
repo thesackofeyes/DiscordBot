5ad0d817a51881121ed8c3351ae76b3127263e1a
thesackofeyes <iamthompson96@gmail.com>
2018-07-07 22:56:25 +0100

    initial RemindMe commit

---
+++ Modules/RemindMe.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class RemindMe : ModuleBase<SocketCommandContext>
+    {
+        [Command("remindme")]
+        public async Task PingAsync(String time)
+        {
+            EmbedBuilder builder = new EmbedBuilder();
+            int minutes = Int32.Parse(time);
+            
+
+
+            builder.WithTitle("Reminder set for "+ time + " Minutes.")
+            .WithDescription(Context.User.Mention + " Will be notified via Direct Message.")
+            .WithColor(Color.Blue);
+
+
+            await Context.User.SendMessageAsync("Ngl bud, this shit don't work yet.");
+            await ReplyAsync("",false,builder.Build());
+        }
+    }
+}
