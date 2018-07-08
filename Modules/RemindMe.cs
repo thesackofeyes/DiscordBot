14916b801a9dc7f53e721b741818db956a641e49
thesackofeyes <iamthompson96@gmail.com>
2018-07-08 20:31:25 +0100

    branch finish

--- Modules/RemindMe.cs
+++ Modules/RemindMe.cs
@@ -11,19 +11,21 @@ namespace DiscordBot.Modules
     public class RemindMe : ModuleBase<SocketCommandContext>
     {
         [Command("remindme")]
-        public async Task PingAsync(String time)
+        public async Task PingAsync(String time) // <- this is how you parse more args on a command
         {
             EmbedBuilder builder = new EmbedBuilder();
             int minutes = Int32.Parse(time);
             
 
-
+            //Builds Chat Message
             builder.WithTitle("Reminder set for "+ time + " Minutes.")
             .WithDescription(Context.User.Mention + " Will be notified via Direct Message.")
             .WithColor(Color.Blue);
 
-
+            //Sends DM
             await Context.User.SendMessageAsync("Ngl bud, this shit don't work yet.");
+
+            //Sends Chat Message
             await ReplyAsync("",false,builder.Build());
         }
     }
